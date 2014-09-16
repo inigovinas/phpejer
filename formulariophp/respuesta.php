9192287314c3f321ef8f7ac3433442b3b654e9d8
inigovinas <inigovinas@hotmail.com>
2014-09-16 08:15:01 +0200

    formulario

---
+++ formulariophp/respuesta.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+    <?php 
+    echo "Nombre: " . $_GET["nombre"] . '<br />';
+    echo "Apellido: " . $_GET["apellido"] . '<br />';
+    echo "Edad: " . $_GET["edad"] . '<br />';
+    echo "Email: " . $_GET["email"];
+     ?>
+
+</body>
+</html>
