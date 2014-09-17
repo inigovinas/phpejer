8a1d05ecf5c7ce21b7d5f4a315702ed426fb12be
inigovinas <inigovinas@hotmail.com>
2014-09-17 09:47:28 +0200

    primer ejercicio propio

---
+++ ejer1negrita/conversion.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+    <?php 
+   	 echo "Texto en negrita: <b>" . $_GET["bolde"]. '</b> <br />';
+   	 echo "Texto en cursiva: <i>" . $_GET["italice"] . '</i> <br />';
+   	 echo "Texto subrayado: <em>" . $_GET["subrayar"] . '</em> <br />';
+    ?>
+</body>
+</html>
