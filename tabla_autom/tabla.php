c5d4414e8c4189608afe583c8b006af44f8cdcec
inigovinas <inigovinas@hotmail.com>
2014-09-17 11:32:03 +0200

    tabla automatica

---
+++ tabla_autom/tabla.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+
+	<table>
+    <?php 
+
+   	 $fil=$_GET["filas"];
+   	 for ($x=0; $x<$fil; $x++) {
+ 		
+	  		echo '<tr>
+	    		<td>'."Jill".'</td>
+	    		<td>'."Smith".'</td>
+	  		</tr>';
+		
+	} 
+    ?>
+
+    </table>
+
+</body>
+</html>
